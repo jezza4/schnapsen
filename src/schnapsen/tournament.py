d3626f07d8d9f01512fcb9addda81f3a1a6729df
jezza4 <jesseh0457@gmail.com>
2024-01-28 22:57:17 +0100

    updated tournament to get winrate

--- src/schnapsen/tournament.py
+++ src/schnapsen/tournament.py
@@ -30,7 +30,7 @@ for a, b in matches:
         winner_id, game_points, score = engine.play_game(
             bots[p[0]], bots[p[1]], random.Random(45)
         )
-        wins[str(winner_id)] += game_points
+        wins[str(winner_id)] += 1 # Set to 1 to get winrate
         
 
         playedgames += 1
